6538e06b76752e8a8c9b427f60fc1d66f036c526
apatel5 <apatel5@DIN57000095.corp.capgemini.com>
2017-02-24 12:48:05 +0530

    updated sql query

--- import.sql
+++ import.sql
@@ -1,21 +1,23 @@
-INSERT INTO `cf_6f9ef31d_e8bb_4683_9e84_8537708795d5`.`services`
-(`id`,
+INSERT INTO `services`
+(
+`id`,
 `bindable`,
 `description`,
 `name`)
 VALUES
-(`1`,
+(
+'1',
 true,
-`Weather app to get and set temperatures for cities`,
-`Weather-Service`);
+'Weather app to get and set temperatures for cities',
+'Weather-Service');
 
-INSERT INTO `cf_6f9ef31d_e8bb_4683_9e84_8537708795d5`.`plans`
+INSERT INTO `plans`
 (`id`,
 `description`,
 `name`,
 `service_id`)
 VALUES
-(`1`,
-`Unlimited free plan for users`,
-`Free-Plan`,
-`1`);
+('1',
+'Unlimited free plan for users',
+'Free-Plan',
+'1');
