0998a413358d2adc685898cb1ea7c1437d7fd99e
apatel5 <apatel5@DIN57000095.corp.capgemini.com>
2017-03-02 15:49:41 +0530

    Added methods to call service methods using rest template

--- src/main/java/com/pcf/training/temperature/broker/mediator/ServiceMediator.java
+++ src/main/java/com/pcf/training/temperature/broker/mediator/ServiceMediator.java
@@ -1,18 +1,22 @@
 package com.pcf.training.temperature.broker.mediator;
 
-import java.sql.Date;
-import java.sql.Time;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.cloudfoundry.com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.cloud.cloudfoundry.com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import com.pcf.training.temperature.broker.config.Env;
+import com.pcf.training.temperature.broker.model.CityWithDate;
 
+@Component
 public class ServiceMediator {
 
 	private static final String TEMPERATURE_SERVICE_PATH = "/city";
@@ -28,52 +32,58 @@ public class ServiceMediator {
 
 		this.restTemplate = restTemplate;
 
-		String helloworldServiceURLTemplate = "http://%s:%s";
-		baseServiceURL = String.format(helloworldServiceURLTemplate, Env.serviceHost);
+		baseServiceURL = "http://" + Env.serviceHost;
 
 		temperatureServiceURL = baseServiceURL + TEMPERATURE_SERVICE_PATH;
 
-		// provide Basic Auth capability
-		/*
-		 * List<ClientHttpRequestInterceptor> interceptors = new
-		 * ArrayList<ClientHttpRequestInterceptor>(); interceptors.add(new
-		 * BasicAuthInterceptor(Env.serviceAdminUserName,
-		 * Env.serviceAdminPassword));
-		 * restTemplate.setInterceptors(interceptors);
-		 */
 	}
 
-	public void createCityTemperature(String hwInstanceId) {
+	public CityWithDate create(CityWithDate cityWithDate) throws JsonProcessingException {
 
 		String url = temperatureServiceURL + "/add";
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
-		HttpEntity<String> entity = new HttpEntity<String>(headers);
 
-		Map<String, Object> urlVars = new HashMap<String, Object>();
-		urlVars.put("hwInstanceId", hwInstanceId);
+		ObjectMapper mapper = new ObjectMapper();
 
-		urlVars.put("date", new Date(System.currentTimeMillis()));
-		urlVars.put("city", "Pune");
+		String jsonInString = mapper.writeValueAsString(cityWithDate);
 
-		Map<String, Object> timelyTemperatureMap = new HashMap<String, Object>();
-		timelyTemperatureMap.put("time", new Time(System.currentTimeMillis()));
-		timelyTemperatureMap.put("temperature", 10);
+		HttpEntity<String> request = new HttpEntity<String>(jsonInString, headers);
 
-		urlVars.put("timelyTemperatures", timelyTemperatureMap);
+		ResponseEntity<CityWithDate> response = restTemplate.exchange(url, HttpMethod.POST, request,
+				CityWithDate.class);
 
-		restTemplate.put(url, entity, urlVars);
+		return response.getBody();
+
 	}
 
-	/*
-	 * public Object getAllCityTemperatures(String hwInstanceId) {
-	 * 
-	 * Map<String, String> urlVars = new HashMap<String, String>();
-	 * 
-	 * String url = temperatureServiceURL + "/getAll"; ResponseEntity
-	 * responseEntity = restTemplate.getForEntity(url, urlVars);
-	 * 
-	 * return responseEntity.getBody(); }
-	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public List<CityWithDate> readAll() {
+
+		String url = temperatureServiceURL + "/getAll";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity request = new HttpEntity(headers);
+
+		ResponseEntity<Object> response = restTemplate.exchange(url, HttpMethod.GET, request, Object.class);
+
+		return (List<CityWithDate>) response.getBody();
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public CityWithDate getDummyObject() {
+
+		String url = temperatureServiceURL + "/getDummyObject";
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity request = new HttpEntity(headers);
+
+		ResponseEntity<CityWithDate> response = restTemplate.exchange(url, HttpMethod.GET, request, CityWithDate.class);
+
+		return response.getBody();
+	}
+
 }
